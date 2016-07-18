46bc9d283a076f9a1883dd172d1b955b960a5e9a
Deepak Mahakale <deepakmahakale@gmail.com>
2016-07-18 20:57:37 +0530

    Basic setup

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe PagesController, type: :controller do
+  describe 'GET #home' do
+    it 'returns http success' do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
