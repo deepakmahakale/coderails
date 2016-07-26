b16a21be333f92179dd06ba321336e84f3cdb130
Deepak Mahakale <deepakmahakale@gmail.com>
2016-07-26 21:34:33 +0530

    Wicked PDF : Table with no break

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are
 # already added.
-Rails.application.config.assets.precompile += %w(vendor.js vendor.css)
+Rails.application.config.assets.precompile +=
+  %w(vendor.js vendor.css pdf_vendor.css)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  namespace :demo, only: [] do
+    resource :wicked_pdf, controller: :wicked_pdf do
+      collection do
+        get :table_with_no_break
+      end
+    end
+  end
 
-  root to: 'pages#home'
+  resource :pages, only: [] do
+    get :home
+    get :demo
+  end
+
+  root to: 'pages#demo'
+
+  match '*path' => redirect('/'), via: :get
 end
