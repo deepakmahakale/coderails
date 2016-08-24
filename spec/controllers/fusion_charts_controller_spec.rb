71487a38cddfb8c4b839f69c173cb674dfcda455
Deepak Mahakale <deepakmahakale@gmail.com>
2016-08-24 20:41:56 +0530

    Refactoring

--- spec/controllers/fusion_charts_controller_spec.rb
+++ spec/controllers/fusion_charts_controller_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe Demo::FusionChartsController, type: :controller do
+RSpec.describe FusionChartsController, type: :controller do
   describe 'GET #column2d' do
     it 'returns http success' do
       get :column2d
