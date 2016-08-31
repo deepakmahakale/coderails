1d060730a3d494b5d4a545e5569df8d5da872f93
Deepak Mahakale <deepakmahakale@gmail.com>
2016-08-31 19:00:58 +0530

    Markdown and Syntax highlighting with redcarpet and rouge

---
+++ config/initializers/haml_markdown.rb
@@ -0,0 +1,56 @@
+require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  # To use Rouge with Redcarpet
+  include Rouge::Plugins::Redcarpet
+  # Overriding Redcarpet method
+  def block_code(code, language)
+    Rouge.highlight(code, language || 'text', 'html')
+  end
+end
+
+module Haml::Filters
+  # Remove the existing Markdown filter
+  remove_filter('Markdown')
+  module Markdown
+    include Haml::Filters::Base
+    def render(text)
+      markdown.render(text)
+    end
+
+    private
+
+    def markdown
+      renderer = HTML.new(render_options)
+      @markdown ||= Redcarpet::Markdown.new(renderer, extensions)
+    end
+
+    def render_options
+      {
+        filter_html:         false,
+        hard_wrap:           true,
+        no_styles:           true,
+        prettify:            true,
+        safe_links_only:     true,
+        with_toc_data:       true
+      }
+    end
+
+    def extensions
+      {
+        autolink:            true,
+        fenced_code_blocks:  true,
+        footnotes:           false,
+        highlight:           true,
+        no_images:           true,
+        no_intra_emphasis:   true,
+        quote:               true,
+        space_after_headers: false,
+        strikethrough:       true,
+        superscript:         true,
+        tables:              true,
+        underline:           true
+      }
+    end
+  end
+end
