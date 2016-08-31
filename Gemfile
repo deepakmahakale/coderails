c6cb2ac7b3d599fccb1137a1dfe25e9cdda8a751
Deepak Mahakale <deepakmahakale@gmail.com>
2016-08-31 19:48:41 +0530

    Demo : Internationalization

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'wkhtmltopdf-binary', '0.12.3'
 gem 'fusioncharts-rails', '0.0.2'
 gem 'redcarpet'
 gem 'rouge'
+gem 'rails-i18n', '~> 4.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.11'
---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# config/initializers/locale.rb
+
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+I18n.available_locales = %i(en es fr)
